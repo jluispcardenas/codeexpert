d28b8af928ceae4fb4bea041fa9221dbe253ddd8
J Cardenas <jluis.pcardenas@gmail.com>
2020-06-07 18:56:02 -0500

    Add sitemap

--- algo/urls.py
+++ algo/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
 #    path('admin/', admin.site.urls),
 
     path('', include('django.contrib.auth.urls')),
+
+    path('sitemap.xml', sitemap, {'sitemaps': {}},
+        name='django.contrib.sitemaps.views.sitemap'),
 
     path('users/',
         include(('profiles.urls', 'profiles'),
